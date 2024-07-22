bb05257b66c17e0dfbb3e08c6b742cd82acfb6b5
Fatihmaull <fatihmaulanamail@gmail.com>
2024-07-22 20:48:44 +0700

    Add files via upload

---
+++ website/data.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+# Data
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 15, 25, 30]
+
+# Plot
+plt.plot(x, y, marker='o')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Example Plot')
+plt.show()
